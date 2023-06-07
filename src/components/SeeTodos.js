08295d9d4f9d7d1bb7fcb72a8e8b07bebe8bd235
Andreas Axelsson <pererikandreas@gmail.com>
2023-06-07 22:03:21 +0200

    added styling to seetodos

--- src/components/SeeTodos.js
+++ src/components/SeeTodos.js
@@ -13,7 +13,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { todos } from '../reducers/todos';
 import { API_URL } from '../utils/urls';
-import './SeeTodos.css';
+import { GlobalStyle, Wrapper, DisplayedTodo, FormInput, FormGroup, FormButton, FormHeader, FormFooter, FlipCard, FlipCardInner, FlipCardFront } from './SeeTodosStyles';
 
 // //////////////////////////////////////////////////////////////////////// //
 // ///////////////////////////// SEE TODOS //////////////////////////////// //
@@ -119,15 +119,15 @@ export const SeeTodos = () => {
   // //////////////////////////////////////////////////////////////////////// //
 
   return (
-    <div className="wrap">
+    <Wrapper>
       {todoList.map((item) => (
         <div key={item._id} className="todo-container">
           <input type="checkbox" id="form_switch" style={{ display: 'none' }} />
           <div className="flipcard">
             <div className={`flipcard-inner${selectedTodo === item._id ? ' flipped' : ''}`}>
               <div className="flipcard-front">
-                <form className="displayed-todo" action="">
-                  <div className="form-group">
+                <DisplayedTodo>
+                  <FormGroup>
                     <div className="cards">
                       <input
                         type="checkbox"
@@ -141,35 +141,35 @@ export const SeeTodos = () => {
                       <p>{item.createdAt}</p>
                       <p>{item.completed}</p>
                       <button type="button" onClick={() => DeleteMessage(item._id)}>Delete</button>
-                      <div className="form-footer">
+                      <FormFooter>
                       Do you want to edit this todo? <label className="label-highlight" htmlFor={`form_switch_${item._id}`} onClick={() => editTodo(item._id)}>EDIT</label>
-                      </div>
+                      </FormFooter>
                     </div>
-                  </div>
-                </form>
+                  </FormGroup>
+                </DisplayedTodo>
               </div>
               <div className="flipcard-back">
                 <form className="login-form" action="">
-                  <div className="form-header">
+                  <FormHeader>
                     <h3>EDIT TODO</h3>
-                  </div>
-                  <div className="form-group">
+                  </FormHeader>
+                  <FormGroup>
                     <input
                       required
                       type="text"
                       name="description"
                       placeholder="Description" />
                     <button type="submit">Submit</button>
-                  </div>
-                  <div className="form-footer">
+                  </FormGroup>
+                  <FormFooter>
                   See updated todo <label className="label-highlight" htmlFor={`form_switch_${item._id}`} onClick={() => backtoTodoList()}>CLICK HERE</label>
-                  </div>
+                  </FormFooter>
                 </form>
               </div>
             </div>
           </div>
         </div>
       ))}
-    </div>
+    </Wrapper>
   );
 };
---
+++ src/components/SeeTodosStyles.js
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+
+export const GlobalStyle = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 350px;
+  height: 400px;
+  margin: 50px auto;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const DisplayedTodo = styled.form`
+  width: 350px;
+  margin: 0 auto;
+  border-radius: 12px;
+  margin-bottom: 23px;
+`;
+
+export const FormInput = styled.input`
+  background: #fafafa;
+  border: 1px solid #eeeeee;
+  padding: 12px;
+  width: 100%;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 1rem;
+  background: #e68989;
+`;
+
+export const FormButton = styled.button`
+  background: #69d2e7;
+  border: 1px solid #ddd;
+  border-radius: 12px;
+  color: #ffffff;
+  padding: 10px;
+  width: 100%;
+  text-transform: uppercase;
+
+  &:hover {
+    background: #69c8e7;
+  }
+`;
+
+export const FormHeader = styled.div`
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+export const FormFooter = styled.div`
+  text-align: center;
+`;
+
+export const FlipCard = styled.div`
+  background-color: transparent;
+  width: 350px;
+  perspective: 1000px;
+`;
+
+export const FlipCardInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+
+  &.flipped {
+    transform: rotateY(180deg);
+  }
+`;
+
+export const FlipCardFront = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  color: black;
+`;
+
+export const FlipCardBack = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  background-color: dodgerblue;
+  transform: rotateY(180deg);
+`;
+
+export const CheckboxInput = styled.input`
+  &:checked + .flipcard .flipcard-inner {
+    transform: rotateY(180deg);
+  }
+`;
+
+export const FormButtonRegister = styled(FormButton)`
+  background: #2c91d8;
+
+  &:hover {
+    background: #2c91a8;
+  }
+`;
+
+export const SwitchForm = styled.div`
+  &:before {
+    content: " ";
+    display: block;
+    background: red;
+  }
+`;
+
+export const LabelHighlight = styled.label`
+  color: blue;
+`;
+
+export const LoginInput = styled.input`
+  border: none;
+  background-color: #e6b8b8;
+  border-radius: 12px;
+  height: 40px;
+  width: 100%;
+`;
+
+export const TodoContainer = styled.div`
+  margin-bottom: 15rem;
+`;
