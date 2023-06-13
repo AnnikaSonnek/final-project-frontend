5636c30ba98ac0389261d7f195428cc109eac2ee
Andreas Axelsson <pererikandreas@gmail.com>
2023-06-13 19:31:27 +0200

    added styled components to tavbar

--- src/components/NavBarStyles.js
+++ src/components/NavBarStyles.js
@@ -1,11 +1,58 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+const mediaQuery = css`
+  @media (max-width: 668px) {
+    .menu-button-container {
+      display: flex;
+    }
+    .menu {
+      position: absolute;
+      top: 0;
+      margin-top: 50px;
+      left: 0;
+      flex-direction: column;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+    }
+    #menu-toggle ~ .menu li {
+      height: 0;
+      margin: 0;
+      padding: 0;
+      border: 0;
+      transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    }
+    #menu-toggle:checked ~ .menu li {
+      border: 1px solid #333;
+      height: 2.5em;
+      padding: 0.5em;
+      transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    }
+    .menu > li {
+      display: flex;
+      justify-content: center;
+      margin: 0;
+      padding: 0.5em 0;
+      width: 100%;
+      color: white;
+      background-color: #222;
+    }
+    .menu > li:not(:last-child) {
+      border-bottom: 1px solid #444;
+    }
+  }
+`;
 export const LogoPicture = styled.div`
   display: flex;
   scale: 15%;
   padding-top: 7px;
-  align-items: center;
+  align-items: flex-start;
   margin-right: auto;
+  ${mediaQuery}
+`;
+
+export const Greeting = styled.p`
+margin-right: 2em;
 `;
 
 export const TopNav = styled.div`
@@ -17,6 +64,7 @@ export const TopNav = styled.div`
   height: 50px;
   padding: 10px;
   padding-left: 0px;
+  ${mediaQuery}
 `;
 
 export const Menu = styled.ul`
@@ -25,11 +73,13 @@ export const Menu = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
+  ${mediaQuery}
 `;
 
 export const MenuItem = styled.li`
   margin: 0 1rem;
   overflow: hidden;
+  ${mediaQuery}
 `;
 
 export const MenuButtonContainer = styled.div`
@@ -40,13 +90,10 @@ export const MenuButtonContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  ${mediaQuery}
 `;
 
-export const MenuToggle = styled.input`
-  display: none;
-`;
-
-export const MenuButton = styled.div`
+export const MenuButton = styled.span`
   display: block;
   background-color: #210440;
   position: absolute;
@@ -55,55 +102,28 @@ export const MenuButton = styled.div`
   transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
   border-radius: 2px;
 
-  &::before {
+  &::before,
+  &::after {
     content: '';
+    display: block;
+    background-color: #210440;
+    position: absolute;
+    height: 4px;
+    width: 30px;
+    transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    border-radius: 2px;
+  }
+
+  &::before {
     margin-top: -8px;
   }
 
   &::after {
-    content: '';
     margin-top: 8px;
   }
 `;
 
-export const MobileMenu = styled(Menu)`
-  position: absolute;
-  top: 0;
-  margin-top: 50px;
-  left: 0;
-  flex-direction: column;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
+export const MenuToggle = styled.input`
+  display: none;
 `;
 
-export const MobileMenuItem = styled(MenuItem)`
-  height: 0;
-  margin: 0;
-  padding: 0;
-  border: 0;
-  transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
-
-  &.checked {
-    border: 1px solid #333;
-    height: 2.5em;
-    padding: 0.5em;
-    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
-  }
-`;
-
-export const MobileMenuButtonContainer = styled(MenuButtonContainer)`
-  display: flex;
-`;
-
-export const MobileMenuButton = styled(MenuButton)`
-  &:before {
-    margin-top: 0px;
-    transform: rotate(405deg);
-  }
-
-  &:after {
-    margin-top: 0px;
-    transform: rotate(-405deg);
-  }
-`;
