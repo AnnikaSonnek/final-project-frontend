6f8d6c872403033c39b0d385160eef8d091b7ad1
AnnikaSonnek <annika.sonnek@gmail.com>
2023-06-15 10:44:15 +0200

    added exception for useEffect dependency

--- src/components/PersonalPage.js
+++ src/components/PersonalPage.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -15,7 +17,6 @@ export const PersonalPage = () => {
       navigate('/')
     }
   }, [accessToken]);
-
 
   return (
     <>
