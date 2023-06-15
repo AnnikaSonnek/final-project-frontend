2ffa3b2fb9d58a154100e48de72ba5d614c81605
AnnikaSonnek <annika.sonnek@gmail.com>
2023-06-15 10:19:04 +0200

    added font

--- src/components/PersonalPage.js
+++ src/components/PersonalPage.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Navbar } from './Navbar';
 import { AvatarPicker } from './AvatarPicker';
 import { ProgressBar } from './ProgressBar';
 import { Levels } from './Levels';
 
 export const PersonalPage = () => {
+  const navigate = useNavigate();
+  const accessToken = useSelector((store) => store.user.accessToken);
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/')
+    }
+  }, [accessToken]);
+
+
   return (
     <>
       <Navbar />
